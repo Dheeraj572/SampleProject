c243c4ca4b558b023d99302ebeb99c85ada90a4c
Dheeraj <kontham.dheeraj@gmail.com>
2020-07-08 09:06:40 +0530

    Added sample data on application loading

--- src/main/java/com/techmojo/entity/Tweet.java
+++ src/main/java/com/techmojo/entity/Tweet.java
@@ -4,14 +4,25 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Tweet {
 	
 	@Id
 	@GeneratedValue
 	private Long id;
 	private String tweet;
+	
+	public Tweet(String tweet) {
+		super();
+		this.tweet = tweet;
+	}
+	
+	
 }
---
+++ src/main/java/com/techmojo/sample/SampleData.java
@@ -0,0 +1,65 @@
+package com.techmojo.sample;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.text.html.parser.Entity;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import com.techmojo.entity.Tweet;
+import com.techmojo.repository.ITweetRepository;
+
+@Component
+public class SampleData {
+
+	    @Autowired
+	    private ITweetRepository iTweetRepository;
+
+	    @EventListener
+	    public void appReady(ApplicationReadyEvent event) {
+	    	List<Tweet> tweetList = new ArrayList<>();
+	    	List<String> sampleList = new ArrayList<>();
+	    	sampleList.add("java");
+	    	sampleList.add("java");
+	    	sampleList.add("java");
+	    	sampleList.add("java");
+	    	sampleList.add("python");
+	    	sampleList.add("python");
+	    	sampleList.add("python");
+	    	sampleList.add("C");
+	    	sampleList.add("C");
+	    	sampleList.add("C");
+	    	sampleList.add("angular");
+	    	sampleList.add("angular");
+	    	sampleList.add("ruby");
+	    	sampleList.add("ruby");
+	    	sampleList.add("springboot");
+	    	sampleList.add("springboot");
+	    	sampleList.add("spring");
+	    	sampleList.add("spring");
+	    	sampleList.add("dotnet");
+	    	sampleList.add("dotnet");
+	    	sampleList.add("google");
+	    	sampleList.add("google");
+	    	sampleList.add("gmail");
+	    	sampleList.add("gmail");
+	    	sampleList.add("facebook");
+	    	sampleList.add("instagram");
+	    	
+	    	for(String s : sampleList) {
+	    		Tweet tweet = new Tweet(s+" is powerful #"+s);
+	    		tweetList.add(tweet);
+	    	}
+	    	
+	    	iTweetRepository.saveAll(tweetList);
+	    
+	    	
+	    	
+	    	
+	    	
+	    }
+}
